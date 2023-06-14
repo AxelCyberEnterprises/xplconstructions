cfac0248d2b60716f07308b53a88de629764d12d
ManOnFireEverytime <55137858+ManOnFireEverytime@users.noreply.github.com>
2023-06-14 06:09:06 +0100

    pics

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 // const top = document.getElementById("top")
-const carousels = document.querySelectorAll(".carousel")
-const btns = document.querySelectorAll(".top-btn")
-const imgs = ["constr 2.png", "bg-img.jpg", "concrete.jpg"]
-const carouselDiv = document.getElementById("carousels")
+const carousels = document.querySelectorAll(".carousel");
+const btns = document.querySelectorAll(".top-btn");
+const imgs = ["constr 2.png", "bg-img.jpg", "concrete.jpg"];
+const carouselDiv = document.getElementById("carousels");
 
-let index = 0
+let index = 0;
 
-let interval = setInterval(run, 3000)
+let interval = setInterval(run, 3000);
 
+btns.forEach((btn) => {
+  let btnId = btn.id;
+  btn.addEventListener("click", () => {
+    clearInterval(interval);
+    removeActiveBtn();
+    btn.classList.add("active");
 
-btns.forEach(btn => {
-
-    let btnId = btn.id
-    btn.addEventListener("click", () => {
-        clearInterval(interval)
-        removeActiveBtn()
-        btn.classList.add("active")
-
-        index = btnId.charAt(btnId.length - 1)
-        setActiveSlide(index - 1) 
-        // resetInterval()
-    })
-})
+    index = btnId.charAt(btnId.length - 1);
+    setActiveSlide(index - 1);
+    // resetInterval()
+  });
+});
 
 // function resetInterval() {
 //     clearInterval(interval)
@@ -29,34 +27,31 @@ btns.forEach(btn => {
 // }
 
 function removeActiveBtn() {
-    btns.forEach(btn => {
-        btn.classList.remove("active")   
-    })
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
 }
-
 
 function setActiveSlide(activeSlide) {
-    carousels.forEach(carousel => {
-        carousel.classList.remove("active")
-    })
-    carousels[activeSlide].classList.add("active")
+  carousels.forEach((carousel) => {
+    carousel.classList.remove("active");
+  });
+  carousels[activeSlide].classList.add("active");
 
-    const bgStyle = `
-        url('../img/${imgs[activeSlide]}') no-repeat center center fixed 
-    `
-    carouselDiv.style.background = bgStyle
-    carouselDiv.style.backgroundSize = "cover"
+  const bgStyle = `
+        url('..img/${imgs[activeSlide]}') no-repeat center center fixed 
+    `;
+  carouselDiv.style.background = bgStyle;
+  carouselDiv.style.backgroundSize = "cover";
 }
-
 
 function run() {
-    index++
-    if(index > carousels.length - 1) {
-        index = 0
-    }
+  index++;
+  if (index > carousels.length - 1) {
+    index = 0;
+  }
 
-    removeActiveBtn()
-    btns[index].classList.add("active")
-    setActiveSlide(index)
+  removeActiveBtn();
+  btns[index].classList.add("active");
+  setActiveSlide(index);
 }
-
